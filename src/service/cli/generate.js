f218b4a8654e029d89423604e219121c6bbf7934
Anna <honiara1951@gmail.com>
2020-02-04 13:04:16 +0200

    created .txt files for data, new fn to read mocks data

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -4,38 +4,13 @@ const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {getRandomInt, shuffle} = require(`../../utils`);
 
+const FILE_PATH_SENTENCES = `./data/sentences.txt`;
+const FILE_PATH_TITLES = `./data/titles.txt`;
+const FILE_PATH_CATEGORIES = `./data/categories.txt`;
+
+
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
 
 const OfferType = {
   offer: `offer`,
@@ -52,28 +27,38 @@ const PictureRange = {
   max: 16,
 };
 
-const getImgFileName = (num) => num < 10 ? `item0${num}.jpg` : `item${num}.jpg`;
-
-const createRandomOffer = () => {
-  const offer = {
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    picture: getImgFileName(getRandomInt(PictureRange.min, PictureRange.max)),
-    description: shuffle(SENTENCES).slice(0, getRandomInt(1, 5)).join(` `),
-    type: Object.keys(OfferType)[getRandomInt(0, Object.keys(OfferType).length - 1)],
-    category: Array(getRandomInt(1, 3)).fill(``)
-      .map(() => CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)])
-      .reduce((acc, it) => !acc.includes(it) ? [...acc, it] : acc, []),
-    sum: getRandomInt(SumRange.min, SumRange.max),
-  };
-
-  return offer;
+const readContent = async (filepath) => {
+  try {
+    const content = await fs.readFile(filepath, `utf-8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
 };
 
-const generateOffers = (amount) => Array(amount).fill({}).map(() => createRandomOffer());
+const getImgFileName = (num) => num < 10 ? `item0${num}.jpg` : `item${num}.jpg`;
+
+const generateOffers = (amount, titles, categories, sentences) => Array(amount).fill({}).map(() => ({
+  title: titles[getRandomInt(0, titles.length - 1)],
+  picture: getImgFileName(getRandomInt(PictureRange.min, PictureRange.max)),
+  description: shuffle(sentences).slice(0, getRandomInt(1, 5)).join(` `),
+  type: Object.keys(OfferType)[getRandomInt(0, Object.keys(OfferType).length - 1)],
+  category: Array(getRandomInt(1, 3)).fill(``)
+      .map(() => categories[getRandomInt(0, categories.length - 1)])
+      .reduce((acc, it) => !acc.includes(it) ? [...acc, it] : acc, []),
+  sum: getRandomInt(SumRange.min, SumRange.max),
+}));
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const [titles, categories, sentences] = await Promise.all([
+      readContent(FILE_PATH_TITLES),
+      readContent(FILE_PATH_CATEGORIES),
+      readContent(FILE_PATH_SENTENCES)
+    ]);
+
     const [amount] = args;
     const offerAmount = Number.parseInt(amount, 10) || DEFAULT_COUNT;
 
@@ -81,7 +66,7 @@ module.exports = {
       console.log(chalk.red(`Не больше 1000 объявлений`));
       return;
     }
-    const content = JSON.stringify(generateOffers(offerAmount));
+    const content = JSON.stringify(generateOffers(offerAmount, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
