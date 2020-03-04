42cf7e4c2eb92b4a91c47688afbbe62e0e3340e0
Anna <honiara1951@gmail.com>
2020-03-04 12:23:31 +0200

    some change

--- src/express/index.js
+++ src/express/index.js
@@ -9,13 +9,14 @@ const registerRoutes = require(`./routes/register`);
 const loginRoutes = require(`./routes/login`);
 
 const PORT = 8080;
-const PUBLIC_DIR = `./markup`;
+const PUBLIC_DIR = `markup`;
 
 const app = express();
 
-app.use(express.static(PUBLIC_DIR));
 app.set(`views`, `./src/express/templates`);
 app.set(`view engine`, `pug`);
+
+app.use(express.static(PUBLIC_DIR));
 
 const Routes = {
   'offers': offersRoutes,
